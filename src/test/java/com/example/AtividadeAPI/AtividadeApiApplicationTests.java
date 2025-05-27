8848a6292a0d6c7148bcab8165f97d799b4c9842
GiovaneCardona <giovanecardona@sptech.school>
2025-05-27 20:22:35 -0300

    initial commit

---
+++ src/test/java/com/example/AtividadeAPI/AtividadeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AtividadeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
